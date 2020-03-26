4488701753c77cc1fcff43b7c730c2db32e4d705
Alex <beliy.san9l@ya.ru>
2020-03-26 20:30:08 +0300

    feat: add class active for navigation

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+// header navigation
+const headerNav = document.querySelector('.header__navigation .navigation__list');
+headerNav.addEventListener('click', addClassActive);
+
+function addClassActive(e) {
+    let target = e.target;
+
+    if (target.tagName == 'A' && !target.classList.contains('active')) {
+        this.querySelectorAll('.navigation__link').forEach(link => {
+            link.classList.remove('active');
+            return;
+        });
+        target.classList.add('active');
+    }
+}
+
+
+// slider
