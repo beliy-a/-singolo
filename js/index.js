9c51a6927afb1378c207b315cd4cf4cf53f70699
Alex <beliy.san9l@ya.ru>
2020-03-31 01:11:08 +0300

    add solution for slider

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 // header navigation
 const headerNav = document.querySelector('.header__navigation .navigation__list');
 headerNav.addEventListener('click', addClassActive);
@@ -16,5 +15,62 @@ function addClassActive(e) {
 }
 
 
+// slider
+const slider = document.querySelector('.slider');
+const sliderItems = document.querySelector('.slider__container').children;
+const prevArrow = document.querySelector('.slider .arrow--back');
+const nextArrow = document.querySelector('.slider .arrow--next');
+let current = 0;
+let isEnabled = true;
 
+prevArrow.addEventListener('click', function () {
+    if (isEnabled) {
+        hideItem('to-right');
+        changeCurrentItem(current - 1);
+        showItem('from-left');
+    }
+});
 
+nextArrow.addEventListener('click', function () {
+    if (isEnabled) {
+        hideItem('to-left');
+        changeCurrentItem(current + 1);
+        showItem('from-right');
+    }
+});
+
+function hideItem(direction) {
+    isEnabled = false;
+    removeModifierForSlider(slider);
+    sliderItems[current].classList.add(direction);
+    sliderItems[current].addEventListener('animationend', function () {
+        this.classList.remove('active', direction);
+    });
+}
+
+function showItem(direction) {
+    addModifierForSlider(sliderItems[current]);
+    sliderItems[current].classList.add('next-item', direction);
+    sliderItems[current].addEventListener('animationend', function () {
+        this.classList.remove('next-item', direction);
+        this.classList.add('active');
+        isEnabled = true;
+    });
+}
+
+function changeCurrentItem(n) {
+    current = (n + sliderItems.length) % sliderItems.length;
+}
+
+function addModifierForSlider(item) {
+    if (item.classList.contains('phone__container--red')) {
+        slider.classList.add('slider--red')
+    } else {
+        slider.classList.add('slider--blue');
+    }
+}
+
+function removeModifierForSlider(item) {
+    let modifier = item.getAttribute('class').split(' ').filter(cls => cls !== 'slider');
+    item.classList.remove(modifier);
+}
